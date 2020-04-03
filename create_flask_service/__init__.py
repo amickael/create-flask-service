61eda34586fd19b315b9973f782ed25f91391606
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-04-02 21:28:21 -0500

    Reorganized

--- create_flask_service/__init__.py
+++ create_flask_service/__init__.py
@@ -3,6 +3,8 @@ import subprocess
 import shutil
 import sys
 
+from halo import Halo
+
 
 __author__ = "Andrew Mickael"
 __version__ = "0.0.1"
@@ -15,10 +17,9 @@ class CreateService:
         self.template_dir = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "template"
         )
-        self.cwd = os.path.join(root_dir, service_name)
 
-    def create_tree(self):
-        # Check if directory already exists, if it does then prompt for overwrite
+        # Check if service directory already exists, if it does then prompt for overwrite
+        self.cwd = os.path.join(root_dir, service_name)
         if os.path.exists(self.cwd):
             proceed = input("Directory already exists, overwrite? [y/N]: ")
             if proceed.lower() == "y":
@@ -27,6 +28,7 @@ class CreateService:
                 print("Stopping")
                 exit()
 
+    def create_tree(self):
         # Copy file structure
         shutil.copytree(
             self.template_dir, self.cwd, ignore=shutil.ignore_patterns("__pycache__")
@@ -38,6 +40,9 @@ class CreateService:
         # Generate venv
         subprocess.run([sys.executable, "-m", "venv", f"{self.cwd}{os.sep}venv"])
 
+        return self
+
+    def install_requirements(self):
         # Install requirements
         subprocess.Popen(
             " && ".join(
@@ -68,58 +73,38 @@ class CreateService:
 
 
 def main():
-    # Collect inputs
-    svc_name = "-".join(input("Service name: ").split(" "))
-    root_dir = input("Root directory (Blank for current directory): ")
+    try:
+        # Prompt input
+        service_name = "-".join(input("Service name: ").split(" "))
+        root_dir = input("Root directory (Blank for current directory): ")
+        service = CreateService(service_name, root_dir)
 
-    # Get working directory and check if already exists. If it does then prompt the user for overwrite
-    cwd = os.path.join(root_dir, svc_name)
-    if os.path.exists(cwd):
-        proceed = input("Directory already exists, overwrite? [y/N]: ")
-        if proceed.lower() == "y":
-            shutil.rmtree(cwd)
-        else:
-            print("Process terminated")
-            return None
+        # Create file structure
+        spinner = Halo(text='Creating file structure')
+        spinner.start()
+        service.create_tree()
+        spinner.succeed()
 
-    # Copy file structure
-    print("\U0001F6E0", "Creating file structure", sep="\t")
-    resource_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "template")
-    shutil.copytree(resource_dir, cwd, ignore=shutil.ignore_patterns("__pycache__"))
+        # Generate venv
+        spinner.start('Generating virtual environment')
+        service.generate_venv()
+        spinner.succeed()
 
-    # Generate venv
-    print("\U0001F3DE", "Generating virtual environment", sep="\t")
-    subprocess.run(["python3", "-m", "venv", f"{cwd}/venv"])
+        # Activate venv and install requirements
+        spinner.start('Installing requirements')
+        service.install_requirements()
+        spinner.succeed()
 
-    # Activate venv and install requirements
-    print("\U0001F4E6", "Installing requirements", sep="\t")
-    activate_cmd = (
-        f"source {cwd}/venv/bin/activate"
-        if os.name == "posix"
-        else fr"{cwd}\Scripts\activate.bat"
-    )
-    install_cmd = f"pip install --upgrade -r {cwd}/requirements.txt"
-    deactivate_cmd = "deactivate"
-    subprocess.Popen(
-        "; ".join([activate_cmd, install_cmd, deactivate_cmd]),
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-        shell=True,
-    ).communicate()
+        # Initialize git
+        spinner.start('Initializing git repository')
+        service.initialize_git()
+        spinner.succeed()
 
-    # Initialize git
-    print("\U0001F680", "Initializing git repository", sep="\t")
-    os.rename(os.path.join(cwd, ".gitignore"), os.path.join(cwd, "..gitignore"))
-    subprocess.Popen(
-        f"cd {cwd}; git init; git add .",
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-        shell=True,
-    ).communicate()
-
-    # Done!
-    print("\U0001F382", "All done!", sep="\t")
-    print("Your new project is located in:", os.path.abspath(cwd), sep="\t")
+        # Done!
+        spinner.stop_and_persist("🎂".encode('utf-8'), 'All done!')
+        print("Your new project is located in:", os.path.abspath(service.cwd), sep="\t")
+    except (KeyboardInterrupt, SystemExit):
+        pass
 
 
 if __name__ == "__main__":

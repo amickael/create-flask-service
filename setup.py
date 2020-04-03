0e0685a625bc4c8e33efd8a6006b5ab6dba71405
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-04-03 10:26:47 -0500

    Added packaging metadata

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup, find_packages
 
 import create_flask_service
 
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
     name="create-flask-service",
     version=create_flask_service.__version__,
     description="Create a Flask microservice with a few keystrokes",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author=create_flask_service.__author__,
     author_email="andrew.mickael@gmail.com",
     license="MIT",
@@ -18,4 +23,11 @@ setup(
         "console_scripts": ["create-flask-service = create_flask_service:main"]
     },
     install_requires=["halo"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+    python_requires=">=3.6",
 )
